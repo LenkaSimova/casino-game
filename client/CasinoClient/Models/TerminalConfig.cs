54bbf28506bf4ddc94f6d90123b8f47b06507d88
Lenka Simova <simova.lenka@gmail.com>
2025-09-05 08:23:16 +0200

    Add backup name

--- client/CasinoClient/Models/TerminalConfig.cs
+++ client/CasinoClient/Models/TerminalConfig.cs
@@ -9,7 +9,7 @@ namespace CasinoClient.Models;
 public class TerminalConfig
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
+    public string Name { get; set; } = "Casino Terminal";
     public List<string> AllowedCommands { get; set; } = new();
     public string Prompt { get; set; } = "casino@terminal:~$ ";
     public string ServerBaseUrl { get; set; } = "http://localhost:5122";
